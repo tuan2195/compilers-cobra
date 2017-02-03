aa57c58193fdde1bba455644c48566dec2b3f961
Tuan Dao <tuandao@login-students.ccs.neu.edu>
2017-02-03 16:40:26 -0500

    Reused test from previous assignment

--- test.ml
+++ test.ml
@@ -15,7 +15,25 @@ let suite =
   t "true1" "if true : 5 else: 10" "5";
   t "false1" "if false : 5 else: 10" "10";
   t "print1" "print(5 - 10)" "-5\n-5";
+
+  t "m1" "5 - 5" "0";
+  t "m2" "5 + 5" "10";
+  t "m3" "5 * 5" "25";
+  t "m4" "5 - 0" "5";
+  t "m5" "5 + 0" "5";
+  t "m6" "5 * 0" "0";
+
+  t "m7" "let x = 5 in x" "5";
+  t "m8" "let x = 5, y = 6 in x + y" "11";
+  t "m9" "let x = 5 + 6 in x" "11";
+  t "m10" "let x = let y = 5 + 6 in y in x - 6" "5";
+  t "m11" "let x = 5 in let y = 5 + x in y" "10";
+  t "m12" "let x = 5, y = 6 in let z = x + y in z" "11";
   t "m13" "let x = 5, y = 6 in let z = let a = x + y in a in z" "11";
+
+  t "m14" "let x = 5 in 5 * x" "25";
+  t "m15" "let x = 5, y = 6 in x * y" "30";
+  t "m16" "let x = 5, y = 6 in let z = let a = x * y in a in z" "30";
   (*t "false2" "if 5 : 5 else: 10" "10";*)
  ]
 ;;
